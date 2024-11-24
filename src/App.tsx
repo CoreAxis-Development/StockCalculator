fb0248bb0f15c0280aff1b2eaf73c0fb741e40c6
KeaganGilmore <keagangilmore@gmail.com>
2024-11-24 22:07:53 +0200

    container space

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Calculator } from './components/Calculator';
-import backgroundSvg from './background.svg';
+import backgroundSvg from './background.svg';  // Update this path to match your actual file structure
 import './App.css';
 
 const App = () => {
