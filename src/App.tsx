8f7eb73a86fcff8f7ab9c07aa14975a64a89d48c
KeaganGilmore <keagangilmore@gmail.com>
2024-11-24 20:43:45 +0200

    gh page fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Calculator } from './components/Calculator';
-import backgroundSvg from './background.svg';  // Update this path to match your actual file structure
+import backgroundSvg from './background.svg';
 import './App.css';
 
 const App = () => {
